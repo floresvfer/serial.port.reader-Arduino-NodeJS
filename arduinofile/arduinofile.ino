6881b10d1958fcebeaf37566d2ab28c5e2aad41e
Fernando Flores <nbafernandoflores8@gmail.com>
2021-08-13 14:48:40 -0600

    arduino hints

--- arduinofile/arduinofile.ino
+++ arduinofile/arduinofile.ino
@@ -2,16 +2,16 @@ void setup() {
   Serial.begin(9600); // Starts the serial communication
 }
 void loop() {
-  Serial.println(getJSON());
-  delay(1000);
+  Serial.println(getJSON()); //Write data on serial port
+  delay(1000); //Wait for 1 sec. to repeat the process
 }
 
 String getJSON(){
     String temperatura = "\"temperatura\": ";
-    temperatura.concat(random(10, 28));
+    temperatura.concat(random(10, 28)); //concat two structures, string + integer
     String viento = "\"viento-vel\": ";
     viento.concat(random(50, 100));
     String humedad = "\"humedad\": ";
     humedad.concat(random(10, 40));
-    return "{"+temperatura+","+viento+","+humedad+"}";
+    return "{"+temperatura+","+viento+","+humedad+"}"; //random json data
 }
